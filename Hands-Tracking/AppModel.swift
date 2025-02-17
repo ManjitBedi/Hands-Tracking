9ee763ebfafe75a6c463ee17564d51e9d87afdee
Manjit Bedi <dev@manjitbedi.com>
2025-02-16 16:05:54 -0800

    Initial Commit

---
+++ Hands-Tracking/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Hands-Tracking
+//
+//  Created by Manjit Bedi on 2025-02-16.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Hands-Tracking/Hands_TrackingApp.swift
@@ -0,0 +1,33 @@
+//
+//  Hands_TrackingApp.swift
+//  Hands-Tracking
+//
+//  Created by Manjit Bedi on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct Hands_TrackingApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
